ff2a1c5f660f4fd97d3fa6e171a18504067922ea
Farid Alfajr <57165716+faridalfajr20@users.noreply.github.com>
2023-10-12 22:33:07 +0700

    Just commit and push

---
+++ app/src/main/java/com/farid/spk/activities/TestActivity.java
@@ -0,0 +1,16 @@
+package com.farid.spk.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.farid.spk.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
